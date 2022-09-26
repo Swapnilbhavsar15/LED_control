808d7ec9a893f1630cc016dad45d5aaae4476f7a
s.bhavsar <s.bhavsar@wmt.team>
2022-09-26 17:55:19 +0200

    LedController pin constants

--- main.py
+++ main.py
@@ -17,24 +17,23 @@ class I2cAbstraction:
 
 
 class LedController:
+    LedEn = 0
+    LedRed1 = 1
+    LedBlue1 = 2
+    LedGreen1 = 3
+    LedWhite1 = 4
+    LedUV1 = 5
+    LedIR1 = 6
+    LedRed2 = 7
+    LedBlue2 = 8
+    LedGreen2 = 9
+    LedWhite2 = 10
+    LedUV2 = 11
+    LedIR2 = 12
+    LedOp1 = 13
+    LedOp2 = 14
     def __init__(self, addr):
         self.addr = addr
-        self.LedEn = 0
-        self.LedRed1 = 1
-        self.LedBlue1 = 2
-        self.LedGreen1 = 3
-        self.LedWhite1 = 4
-        self.LedUV1 = 5
-        self.LedIR1 = 6
-        self.LedRed2 = 7
-        self.LedBlue2 = 8
-        self.LedGreen2 = 9
-        self.LedWhite2 = 10
-        self.LedUV2 = 11
-        self.LedIR2 = 12
-        self.LedOp1 = 13
-        self.LedOp2 = 14
-
 
     def read(self, data, memaddr, addr_size, timeout=5000):
         PybI2c.i2c_read(data, self.addr, memaddr, addr_size)
@@ -48,6 +47,10 @@ class LedController:
     def send(self, send, timeout=5000):
         PybI2c.i2c_send(send, self.addr)
 
-class LedCell(LedController):
-    def __init__(self, addr):
-        super(). __init__(addr)
+class LedCell:
+    def __init__(self, ledcontroller):
+        self.ledcontroller = ledcontroller
+    def setred(self, value, isTrue):
+
+
+
