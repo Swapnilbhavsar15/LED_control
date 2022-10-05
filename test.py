e36433366270b402d76e457570330969cec969c0
s.bhavsar <s.bhavsar@wmt.team>
2022-10-05 15:35:45 +0200

    Code cleaned using _setPWM function and description added for various functions

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def test_1():
     ledcontroller = LedController(5, i2c)
     ledcontroller.setfreq(200)
     led_IR = LedCell(ledcontroller, True)
-    led_IR.setblue(30)
+    led_IR.setir(30)
 
 
 
