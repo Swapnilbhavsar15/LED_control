e7e6631246d9d004ae84d277cb4dea7736432241
s.bhavsar <s.bhavsar@wmt.team>
2022-10-05 15:33:08 +0200

    Code cleaned using _setPWM function and description added for various functions

--- ledcontrol.py
+++ ledcontrol.py
@@ -61,17 +61,17 @@ class LedCell:
         self.ledcontroller.bus.i2c_writeto_mem(i2c_address, register_address, new_val.to_bytes(2, byteorder='little'))
 
     # Add delay parameter if need to be used
-    def _setPWM(self, register_address, value):
+    def _setpwm(self, register_address, value):
         # if value+delay <= 100:
         self._setmain(register_address, 0x00)
         self._setmain(register_address + 0x02, value)
         # elif value+delay > 100:
-            # self.setmain(register_address, delay)
-            # self.setmain(register_address + 0x02, delay+value-100)
+        # self.setmain(register_address, delay)
+        # self.setmain(register_address + 0x02, delay+value-100)
 
     def _get_register(self, pin):
-        addr_dict = { 1: 0x0A, 2: 0x0E, 3: 0x12, 4: 0x16, 5: 0x1A, 6: 0x1E,
-                      7: 0x22, 8: 0x26, 9: 0x2A, 10: 0x2E, 11: 0x32, 12: 0x36 }
+        addr_dict = {1: 0x0A, 2: 0x0E, 3: 0x12, 4: 0x16, 5: 0x1A, 6: 0x1E,
+                     7: 0x22, 8: 0x26, 9: 0x2A, 10: 0x2E, 11: 0x32, 12: 0x36}
         return addr_dict[pin]
 
     def setred(self, value):
@@ -79,68 +79,67 @@ class LedCell:
         The function turns Red Led on with desired intensity value
         :param value: 0-100 Led on time
         """
-         # delay = 10
+        # delay = 10
         if self.is_first_on_led_controller:
             pin = LedController.LedRed1
         else:
             pin = LedController.LedRed2
         register_address = self._get_register(pin)
-        self._setPWM(register_address, value)
+        self._setpwm(register_address, value)
 
     def setblue(self, value):
         """
         The function turns Blue Led on with desired intensity value
         :param value: 0-100 Led on time
         """
-         # delay = 20
+        # delay = 20
         if self.is_first_on_led_controller:
             pin = LedController.LedBlue1
         else:
             pin = LedController.LedBlue2
         register_address = self._get_register(pin)
-        self._setPWM(register_address, value)
-
+        self._setpwm(register_address, value)
 
     def setgreen(self, value):
         """
         The function turns Green Led on with desired intensity value
         :param value: 0-100 Led on time
         """
-         # delay = 30
+        # delay = 30
         if self.is_first_on_led_controller:
             pin = LedController.LedGreen1
         else:
             pin = LedController.LedGreen2
         register_address = self._get_register(pin)
-        self._setPWM(register_address, value)
+        self._setpwm(register_address, value)
 
     def setwhite(self, value):
         """
         The function turns White Led on with desired intensity value
         :param value: 0-100 Led on time
         """
-         # delay = 40
+        # delay = 40
         if self.is_first_on_led_controller:
             pin = LedController.LedWhite1
         else:
             pin = LedController.LedWhite2
         register_address = self._get_register(pin)
-        self._setPWM(register_address, value)
+        self._setpwm(register_address, value)
 
-    def setUV(self, value):
+    def setuv(self, value):
         """
         The function turns Ulatraviolet Led on with desired intensity value
         :param value: 0-100 Led on time
         """
-         # delay = 50
+        # delay = 50
         if self.is_first_on_led_controller:
             pin = LedController.LedUV1
         else:
             pin = LedController.LedUV2
         register_address = self._get_register(pin)
-        self._setPWM(register_address, value)
+        self._setpwm(register_address, value)
 
-    def setIR(self, value):
+    def setir(self, value):
         """
         The function turns Infrared Led on with desired intensity value
         :param value: 0-100 Led on time
@@ -151,6 +150,4 @@ class LedCell:
         else:
             pin = LedController.LedIR2
         register_address = self._get_register(pin)
-        self._setPWM(register_address, value)
-
-
+        self._setpwm(register_address, value)
