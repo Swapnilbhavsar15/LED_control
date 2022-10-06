965b5de8021b56db5fb1fff837511e1359dbcfe8
s.bhavsar <s.bhavsar@wmt.team>
2022-10-06 17:17:51 +0200

    Output enable function added in LedController created.

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from ledcontrol import LedController,LedCell
 
 def test_1():
     i2c = I2CDummy()
-    ledcontroller = LedController(5, i2c, True)
+    ledcontroller = LedController(5, i2c, False)
     ledcontroller.setfreq(200)
     led_IR = LedCell(ledcontroller, True)
     led_IR.setir(90)
